c3fee3401a27307fc7956b85791877f7da2a3351
Gaius Nale <gaiusnale@gmail.com>
2024-06-24 16:07:59 +0100

    Defined the colour of the game

--- beta.py
+++ beta.py
@@ -4,5 +4,13 @@ import numpy as np
 
 pygame.init()
 
+# Color constants 
 WHITE = (255, 255, 255)
-GRAY = ()
+GRAY = (128, 128, 128)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLACK = (0, 0, 0)
+
+WIDTH = 300
+HEIGHT = 300
+
